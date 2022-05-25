f7f89d0d7b8bb3d1b78f73eb1d0a0311335584c1
telolahy <telodan9@gmail.com>
2022-05-25 13:53:07 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return ( 
+    <React.Fragment>
+      <h1 className=" text-red-600">bonjour </h1>
+    </React.Fragment>
+   );
+}
+
+export default App;
